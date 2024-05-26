ad0683422994838b26a4cff399cabe80c79f060c
Gideon Ritter <gideon.ritter@enymo.com>
2024-05-26 17:56:40 +0200

    Fix wrong exports.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,8 +1,9 @@
 export { default as CheckboxList, useCheckboxList } from "./Components/CheckboxList";
-export { default as GlissadeInput, default as useGlissadeInput } from "./Components/GlissadeInput";
+export { default as GlissadeInput } from "./Components/GlissadeInput";
 export { default as RadioGroup, useRadioGroup } from "./Components/RadioGroup";
 export { default as useGlissadeButton } from "./Hooks/GlissadeButtonHook";
 export { default as useGlissadeChoice } from "./Hooks/GlissadeChoiceHook";
+export { default as useGlissadeInput } from "./Hooks/GlissadeInputHook";
 export * from "./types";
 export * from "./util";
 
